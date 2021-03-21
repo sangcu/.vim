7e0dd13b156ea464cf2b128606781ab2cf31a7bf
sangcu <sangcu@users.noreply.github.com>
2021-03-21 15:34:47 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
 sudo apt install fd-find
 npm -g install js-beautify
 export FZF_DEFAULT_COMMAND="fdfind --type file"
+echo "export FZF_DEFAULT_COMMAND=\"fdfind --type file\"" >> ~/.bashrc
+echo "export GIT_EDITOR=vim" >> ~/.bashrc
